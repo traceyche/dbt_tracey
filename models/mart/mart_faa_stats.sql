140ef1fd1dfab7e6a0b4e178e4eeef78b3121a1a
traceyche <traceycheking@gmail.com>
2026-02-26 12:43:17 +0100

    update faa 2

--- models/mart/mart_faa_stats.sql
+++ models/mart/mart_faa_stats.sql
@@ -33,14 +33,14 @@ select
     prep_airports.name as airport_name,
     prep_airports.city,
     prep_airports.country,
-    coalesce(departures.unique_departure_connections,0) as unique_departure_connections,
-    coalesce(arrivals.unique_arrival_connections,0) as unique_arrival_connections,
-    coalesce(departures.total_departures,0) + coalesce(arrivals.total_arrivals,0) as total_flights_planned,
-    coalesce(departures.cancelled_departures,0) + coalesce(arrivals.cancelled_arrivals,0) as total_cancelled,
-    coalesce(departures.diverted_departures,0) + coalesce(arrivals.diverted_arrivals,0) as total_diverted,
-    coalesce(departures.completed_departures,0) + coalesce(arrivals.completed_arrivals,0) as total_completed,
-    ((coalesce(departures.unique_airplanes_departed,0) + coalesce(arrivals.unique_airplanes_arrived,0))/2) as avg_unique_airplanes,
-    ((coalesce(departures.unique_airlines_departed,0) + coalesce(arrivals.unique_airlines_arrived,0))/2) as avg_unique_airlines
+    coalesce(departures.unique_departure_connections, 0) as unique_departure_connections,
+    coalesce(arrivals.unique_arrival_connections, 0) as unique_arrival_connections,
+    coalesce(departures.total_departures, 0) + coalesce(arrivals.total_arrivals, 0) as total_flights_planned,
+    coalesce(departures.cancelled_departures, 0) + coalesce(arrivals.cancelled_arrivals, 0) as total_cancelled,
+    coalesce(departures.diverted_departures, 0) + coalesce(arrivals.diverted_arrivals, 0) as total_diverted,
+    coalesce(departures.completed_departures, 0) + coalesce(arrivals.completed_arrivals, 0) as total_completed,
+    (coalesce(departures.unique_airplanes_departed, 0) + coalesce(arrivals.unique_airplanes_arrived, 0))/2 as avg_unique_airplanes,
+    (coalesce(departures.unique_airlines_departed, 0) + coalesce(arrivals.unique_airlines_arrived, 0))/2 as avg_unique_airlines
 from {{ ref('prep_airports') }}
 left join departures on prep_airports.faa = departures.airport_code
 left join arrivals on prep_airports.faa = arrivals.airport_code
