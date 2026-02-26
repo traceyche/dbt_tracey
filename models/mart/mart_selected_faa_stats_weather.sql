a8ce337f119c087c9d3f70dfa8d7e7804343d02c
traceyche <traceycheking@gmail.com>
2026-02-26 12:00:16 +0100

    update

--- models/mart/mart_selected_faa_stats_weather.sql
+++ models/mart/mart_selected_faa_stats_weather.sql
@@ -11,29 +11,29 @@ airports as (
 ),
 departures as (
     select
-        origin_airport_id as airport_id,
+        origin as airport_code,
         flight_date,
-        count(distinct destination_airport_id) as unique_departure_connections,
+        count(distinct dest) as unique_departure_connections,
         count(*) as total_departures,
         count(case when cancelled = 1 then 1 end) as cancelled_departures,
         count(case when diverted = 1 then 1 end) as diverted_departures,
         count(case when cancelled = 0 and diverted = 0 then 1 end) as completed_departures,
-        count(distinct airplane_id) as unique_airplanes_departed,
-        count(distinct airline_id) as unique_airlines_departed
+        count(distinct tail_number) as unique_airplanes_departed,
+        count(distinct airline) as unique_airlines_departed
     from flights
     group by 1,2
 ),
 arrivals as (
     select
-        destination_airport_id as airport_id,
+        dest as airport_code,
         flight_date,
-        count(distinct origin_airport_id) as unique_arrival_connections,
+        count(distinct origin) as unique_arrival_connections,
         count(*) as total_arrivals,
         count(case when cancelled = 1 then 1 end) as cancelled_arrivals,
         count(case when diverted = 1 then 1 end) as diverted_arrivals,
         count(case when cancelled = 0 and diverted = 0 then 1 end) as completed_arrivals,
-        count(distinct airplane_id) as unique_airplanes_arrived,
-        count(distinct airline_id) as unique_airlines_arrived
+        count(distinct tail_number) as unique_airplanes_arrived,
+        count(distinct airline) as unique_airlines_arrived
     from flights
     group by 1,2
 )
@@ -51,14 +51,15 @@ select
     coalesce(d.completed_departures,0)+coalesce(ar.completed_arrivals,0) as total_completed,
     ((coalesce(d.unique_airplanes_departed,0)+coalesce(ar.unique_airplanes_arrived,0))/2) as avg_unique_airplanes,
     ((coalesce(d.unique_airlines_departed,0)+coalesce(ar.unique_airlines_arrived,0))/2) as avg_unique_airlines,
-    w.min_temperature,
-    w.max_temperature,
-    w.precipitation,
-    w.snowfall,
+    w.min_temp_c,
+    w.max_temp_c,
+    w.avg_temp_c,
+    w.precipitation_mm,
+    w.max_snow_mm,
     w.avg_wind_direction,
-    w.avg_wind_speed,
-    w.wind_peak_gust
+    w.avg_wind_speed_kmh,
+    w.wind_peakgust_kmh
 from airports a
-left join departures d on a.airport_id = d.airport_id and d.flight_date = w.date
-left join arrivals ar on a.airport_id = ar.airport_id and ar.flight_date = w.date
-left join weather w on a.airport_id = w.station_id and w.date = d.flight_date
+left join departures d on a.airport_code = d.airport_code and d.flight_date = w.date
+left join arrivals ar on a.airport_code = ar.airport_code and ar.flight_date = w.date
+left join weather w on a.airport_code = w.airport_code and w.date = d.flight_date
