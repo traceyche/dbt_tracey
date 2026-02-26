229e02169eba964e07e57090abadcc19bf8c83b6
traceyche <traceycheking@gmail.com>
2026-02-26 12:11:08 +0100

    final changes

--- models/mart/mart_selected_faa_stats_weather.sql
+++ models/mart/mart_selected_faa_stats_weather.sql
@@ -1,15 +1,5 @@
 {{ config(materialized='table') }}
-
-with flights as (
-    select * from {{ ref('prep_flights') }}
-),
-weather as (
-    select * from {{ ref('prep_weather_daily') }}
-),
-airports as (
-    select * from {{ ref('prep_airports') }}
-),
-departures as (
+with departures as (
     select
         origin as airport_code,
         flight_date,
@@ -20,8 +10,8 @@ departures as (
         count(case when cancelled = 0 and diverted = 0 then 1 end) as completed_departures,
         count(distinct tail_number) as unique_airplanes_departed,
         count(distinct airline) as unique_airlines_departed
-    from flights
-    group by 1,2
+    from {{ ref('prep_flights') }}
+    group by origin, flight_date
 ),
 arrivals as (
     select
@@ -34,33 +24,36 @@ arrivals as (
         count(case when cancelled = 0 and diverted = 0 then 1 end) as completed_arrivals,
         count(distinct tail_number) as unique_airplanes_arrived,
         count(distinct airline) as unique_airlines_arrived
-    from flights
-    group by 1,2
+    from {{ ref('prep_flights') }}
+    group by dest, flight_date
+),
+weather as (
+    select * from {{ ref('prep_weather_daily') }}
 )
 
 select
-    a.airport_code,
-    a.airport_name,
-    a.city,
-    a.country,
-    coalesce(d.flight_date, ar.flight_date, w.date) as flight_date,
-    coalesce(d.unique_departure_connections,0) as unique_departure_connections,
-    coalesce(ar.unique_arrival_connections,0) as unique_arrival_connections,
-    coalesce(d.total_departures,0)+coalesce(ar.total_arrivals,0) as total_flights_planned,
-    coalesce(d.cancelled_departures,0)+coalesce(ar.cancelled_arrivals,0) as total_cancelled,
-    coalesce(d.diverted_departures,0)+coalesce(ar.diverted_arrivals,0) as total_diverted,
-    coalesce(d.completed_departures,0)+coalesce(ar.completed_arrivals,0) as total_completed,
-    ((coalesce(d.unique_airplanes_departed,0)+coalesce(ar.unique_airplanes_arrived,0))/2) as avg_unique_airplanes,
-    ((coalesce(d.unique_airlines_departed,0)+coalesce(ar.unique_airlines_arrived,0))/2) as avg_unique_airlines,
-    w.min_temp_c,
-    w.max_temp_c,
-    w.avg_temp_c,
-    w.precipitation_mm,
-    w.max_snow_mm,
-    w.avg_wind_direction,
-    w.avg_wind_speed_kmh,
-    w.wind_peakgust_kmh
-from airports a
-left join departures d on a.airport_code = d.airport_code
-left join arrivals ar on a.airport_code = ar.airport_code and ar.flight_date = d.flight_date
-left join weather w on a.airport_code = w.airport_code and w.date = coalesce(d.flight_date, ar.flight_date)
+    airport_code,
+    airport_name,
+    city,
+    country,
+    coalesce(departures.flight_date, arrivals.flight_date, weather.date) as flight_date,
+    coalesce(unique_departure_connections,0) as unique_departure_connections,
+    coalesce(unique_arrival_connections,0) as unique_arrival_connections,
+    coalesce(total_departures,0) + coalesce(total_arrivals,0) as total_flights_planned,
+    coalesce(cancelled_departures,0) + coalesce(cancelled_arrivals,0) as total_cancelled,
+    coalesce(diverted_departures,0) + coalesce(diverted_arrivals,0) as total_diverted,
+    coalesce(completed_departures,0) + coalesce(completed_arrivals,0) as total_completed,
+    ((coalesce(unique_airplanes_departed,0) + coalesce(unique_airplanes_arrived,0))/2) as avg_unique_airplanes,
+    ((coalesce(unique_airlines_departed,0) + coalesce(unique_airlines_arrived,0))/2) as avg_unique_airlines,
+    min_temp_c,
+    max_temp_c,
+    avg_temp_c,
+    precipitation_mm,
+    max_snow_mm,
+    avg_wind_direction,
+    avg_wind_speed_kmh,
+    wind_peakgust_kmh
+from {{ ref('prep_airports') }}
+left join departures on airport_code = departures.airport_code
+left join arrivals on airport_code = arrivals.airport_code and arrivals.flight_date = departures.flight_date
+left join weather on airport_code = weather.airport_code and weather.date = coalesce(departures.flight_date, arrivals.flight_date)
