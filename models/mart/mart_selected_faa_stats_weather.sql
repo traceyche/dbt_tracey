ae583ec67c580ed1f43b0ee1c5662e556fa654b4
traceyche <traceycheking@gmail.com>
2026-02-26 12:14:57 +0100

    error update

--- models/mart/mart_selected_faa_stats_weather.sql
+++ models/mart/mart_selected_faa_stats_weather.sql
@@ -1,4 +1,5 @@
 {{ config(materialized='table') }}
+
 with departures as (
     select
         origin as airport_code,
@@ -32,28 +33,30 @@ weather as (
 )
 
 select
-    airport_code,
-    airport_name,
-    city,
-    country,
+    prep_airports.airport_code,
+    prep_airports.airport_name,
+    prep_airports.city,
+    prep_airports.country,
     coalesce(departures.flight_date, arrivals.flight_date, weather.date) as flight_date,
-    coalesce(unique_departure_connections,0) as unique_departure_connections,
-    coalesce(unique_arrival_connections,0) as unique_arrival_connections,
-    coalesce(total_departures,0) + coalesce(total_arrivals,0) as total_flights_planned,
-    coalesce(cancelled_departures,0) + coalesce(cancelled_arrivals,0) as total_cancelled,
-    coalesce(diverted_departures,0) + coalesce(diverted_arrivals,0) as total_diverted,
-    coalesce(completed_departures,0) + coalesce(completed_arrivals,0) as total_completed,
-    ((coalesce(unique_airplanes_departed,0) + coalesce(unique_airplanes_arrived,0))/2) as avg_unique_airplanes,
-    ((coalesce(unique_airlines_departed,0) + coalesce(unique_airlines_arrived,0))/2) as avg_unique_airlines,
-    min_temp_c,
-    max_temp_c,
-    avg_temp_c,
-    precipitation_mm,
-    max_snow_mm,
-    avg_wind_direction,
-    avg_wind_speed_kmh,
-    wind_peakgust_kmh
+    coalesce(departures.unique_departure_connections,0) as unique_departure_connections,
+    coalesce(arrivals.unique_arrival_connections,0) as unique_arrival_connections,
+    coalesce(departures.total_departures,0) + coalesce(arrivals.total_arrivals,0) as total_flights_planned,
+    coalesce(departures.cancelled_departures,0) + coalesce(arrivals.cancelled_arrivals,0) as total_cancelled,
+    coalesce(departures.diverted_departures,0) + coalesce(arrivals.diverted_arrivals,0) as total_diverted,
+    coalesce(departures.completed_departures,0) + coalesce(arrivals.completed_arrivals,0) as total_completed,
+    ((coalesce(departures.unique_airplanes_departed,0) + coalesce(arrivals.unique_airplanes_arrived,0))/2) as avg_unique_airplanes,
+    ((coalesce(departures.unique_airlines_departed,0) + coalesce(arrivals.unique_airlines_arrived,0))/2) as avg_unique_airlines,
+    weather.min_temp_c,
+    weather.max_temp_c,
+    weather.avg_temp_c,
+    weather.precipitation_mm,
+    weather.max_snow_mm,
+    weather.avg_wind_direction,
+    weather.avg_wind_speed_kmh,
+    weather.wind_peakgust_kmh
 from {{ ref('prep_airports') }}
-left join departures on airport_code = departures.airport_code
-left join arrivals on airport_code = arrivals.airport_code and arrivals.flight_date = departures.flight_date
-left join weather on airport_code = weather.airport_code and weather.date = coalesce(departures.flight_date, arrivals.flight_date)
+left join departures on prep_airports.airport_code = departures.airport_code
+left join arrivals on prep_airports.airport_code = arrivals.airport_code
+    and arrivals.flight_date = departures.flight_date
+left join weather on prep_airports.airport_code = weather.airport_code
+    and weather.date = coalesce(departures.flight_date, arrivals.flight_date)
